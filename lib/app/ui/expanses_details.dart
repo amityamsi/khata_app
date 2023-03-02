7c459e119f0b1d5a20d1c6cf6bc533c466a2184f
Amit Bahadur <amitbahadur1994@gmail.com>
2023-03-03 01:46:14 +0530

    latest code

---
+++ lib/app/ui/expanses_details.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ExpansesDetailScreen extends StatelessWidget {
+  const ExpansesDetailScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- lib/app/ui/home_screen.dart
+++ lib/app/ui/home_screen.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
 import 'package:khata_app/app/widgets/custom_list_widget.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -10,57 +8,64 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Dashboard"),
+        title: const Text("Dashboard"),
         actions: [ElevatedButton(onPressed: () {}, child: Text("Bal: 1002"))],
       ),
       drawer: SafeArea(
         child: Drawer(
-          child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 50),
+            child: SizedBox(
+              height: 500,
+              child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
-                    Icon(Icons.cloud_upload),
-                    Text("Backup"),
-                  ],
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    Icon(Icons.restore),
-                    Text("Restore"),
-                  ],
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    Icon(Icons.password_rounded),
-                    Text("Change Password"),
-                  ],
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    Icon(Icons.currency_exchange_outlined),
-                    Text("Change Currency"),
-                  ],
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    Icon(Icons.article_outlined),
-                    Text("Terms & Conditions"),
-                  ],
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    Icon(Icons.share),
-                    Text("Share"),
-                  ],
-                ),
-              ]),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Icon(Icons.cloud_upload),
+                        Text("Backup"),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Icon(Icons.restore),
+                        Text("Restore"),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Icon(Icons.password_rounded),
+                        Text("Change Password"),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Icon(Icons.currency_exchange_outlined),
+                        Text("Change Currency"),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Icon(Icons.article_outlined),
+                        Text("Terms & Conditions"),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Icon(Icons.share),
+                        Text("Share"),
+                      ],
+                    ),
+                  ]),
+            ),
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
@@ -69,5 +74,9 @@ class HomeScreen extends StatelessWidget {
       ),
       body: CustomTileListWidget(),
     );
+  }
+
+  Widget addNewExpanse() {
+    return Container();
   }
 }
