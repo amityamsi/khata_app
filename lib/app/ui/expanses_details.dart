0c2c834b6c447a97633c4481bf80a220894d6446
Amit Bahadur <amitbahadur1994@gmail.com>
2023-03-13 02:25:20 +0530

    update side drawer

--- lib/app/ui/expanses_details.dart
+++ lib/app/ui/expanses_details.dart
@@ -5,6 +5,16 @@ class ExpansesDetailScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SafeArea(
+        child: Scaffold(
+      appBar: AppBar(
+        title: const Text("Account Name"),
+        actions: [
+          const Text("Bal: ₹501.0"),
+          IconButton(onPressed: () {}, icon: Icon(Icons.share))
+        ],
+      ),
+      body: Column(),
+    ));
   }
 }
--- lib/app/ui/home_screen.dart
+++ lib/app/ui/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:khata_app/app/constants/colors.dart';
 import 'package:khata_app/app/widgets/custom_list_widget.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -6,77 +7,120 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var height = MediaQuery.of(context).size.height;
+    var width = MediaQuery.of(context).size.width;
+
     return Scaffold(
       appBar: AppBar(
         title: const Text("Dashboard"),
         actions: [ElevatedButton(onPressed: () {}, child: Text("Bal: 1002"))],
       ),
       drawer: SafeArea(
-        child: Drawer(
-          child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 50),
-            child: SizedBox(
-              height: 500,
-              child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+        child: SizedBox(
+          height: height * .5,
+          child: Drawer(
+            shadowColor: AppColors.black,
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 50),
+              child: Container(
+                child: Padding(
+                  padding: EdgeInsets.only(left: width * .1),
+                  child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                       children: [
-                        Icon(Icons.cloud_upload),
-                        Text("Backup"),
-                      ],
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                        Icon(Icons.restore),
-                        Text("Restore"),
-                      ],
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                        Icon(Icons.password_rounded),
-                        Text("Change Password"),
-                      ],
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                        Icon(Icons.currency_exchange_outlined),
-                        Text("Change Currency"),
-                      ],
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                        Icon(Icons.article_outlined),
-                        Text("Terms & Conditions"),
-                      ],
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                        Icon(Icons.share),
-                        Text("Share"),
-                      ],
-                    ),
-                  ]),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Icon(Icons.cloud_upload),
+                            Text("Backup"),
+                          ],
+                        ),
+                        Divider(),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Icon(Icons.restore),
+                            Text("Restore"),
+                          ],
+                        ),
+                        Divider(),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Icon(Icons.password_rounded),
+                            Text("Change Password"),
+                          ],
+                        ),
+                        Divider(),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Icon(Icons.currency_exchange_outlined),
+                            Text("Change Currency"),
+                          ],
+                        ),
+                        Divider(),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Icon(Icons.article_outlined),
+                            Text("Terms & Conditions"),
+                          ],
+                        ),
+                        Divider(),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Icon(Icons.share),
+                            Text("Share"),
+                          ],
+                        ),
+                      ]),
+                ),
+              ),
             ),
           ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          addNewExpanse(context, width);
+        },
         child: Icon(Icons.add),
       ),
       body: CustomTileListWidget(),
     );
   }
 
-  Widget addNewExpanse() {
-    return Container();
+  addNewExpanse(context, width) {
+    showDialog<void>(
+      context: context,
+      barrierDismissible: true, // user must tap button!
+      builder: (BuildContext context) {
+        return AlertDialog(
+          alignment: Alignment.center,
+          shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(20.0))),
+          title: const Text('Add a new account'),
+          content: SingleChildScrollView(
+            child: Column(
+              children: [
+                TextField(
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(), hintText: "Account Name"),
+                ),
+                Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      TextButton(onPressed: () {}, child: Text("Cancel")),
+                      ElevatedButton(onPressed: () {}, child: Text("Save"))
+                    ]),
+              ],
+            ),
+          ),
+        );
+      },
+    );
   }
 }
